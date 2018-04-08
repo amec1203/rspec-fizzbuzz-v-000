960b2d21769cbc81523a8680a4d829089de255d3
Angela Campion <campion.angela@gmail.com>
2018-04-08 23:29:50 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 int = gets.chomp
 def fizzbuzz(int)
   if int % 3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif int % 5 == 0
-    puts "Buzz"
+    "Buzz"
   elsif int % 3 && 5 == 0
-    puts "FizzBuzz"
+    "FizzBuzz"
   else int !(% 3 == 0 || 5 == 0)
-    puts "nil"
-  end 
+    "nil"
+  end
 end
