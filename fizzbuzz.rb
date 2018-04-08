3b1c1552edfd6dab92c63e5ef846ea1b37b59758
Angela Campion <campion.angela@gmail.com>
2018-04-08 23:34:31 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ def fizzbuzz(int)
     puts "Fizz"
   elsif int % 5 == 0
     puts "Buzz"
-  elsif int % 3 == 0 && 5 == 0
+  elsif int % 3 == 0 && % 5 == 0
     puts "FizzBuzz"
   else int !(% 3 == 0 || 5 == 0)
     "nil"
