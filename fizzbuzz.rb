f5de2314efc144e7c9a24d93ab6a08ff4dd19cab
Angela Campion <campion.angela@gmail.com>
2018-04-09 00:25:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,13 @@
 def fizzbuzz(int)
   if int % 3 == 0
-    return "Fizz"
+      if int % 3 == 0 && int % 5 == 0
+        return "FizzBuzz"
+      else
+        return "Fizz"
+      end
   end
   if int % 5 == 0
     return "Buzz"
-  end
-  if int % 3 == 0 && int % 5 == 0
-    return "FizzBuzz"
   else
     return nil
   end
